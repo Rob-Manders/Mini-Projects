f540131cc442039c18c2632192947272f9e8f5bf
Robert Manders <31056731+Rob-Manders@users.noreply.github.com>
2021-02-21 14:16:11 +0000

    Create project - Write markup and styling.

---
+++ Contrast Ratio/app.js
@@ -0,0 +1,13 @@
+
+// DOM
+const colourDisplay = {
+	bacgkround:	document.getElementById('colour-window'),
+	foreground: document.getElementById('colour-window__foreground-colour')
+}
+
+const backgroundInput = document.getElementById('background-input')
+const foregroundInput = document.getElementById('foreground-input')
+const submitInputs = document.getElementById('submit-inputs')
+
+const resultDisplay = document.getElementById('result-display')
+
